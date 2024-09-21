dec3ecc90259e874af2b3ee69b0af9a00b999b4f
DC_Jay <Harrisj22412@hocking.edu>
2024-09-21 12:48:07 +0000

    Create a To-Do Model

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/todo'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(80),unique=True, nullable=False)
